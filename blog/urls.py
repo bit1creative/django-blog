3fcdfeb6cba5e65d29604d3eb041bfd31b68a687
bit1creative <hrytsenko.svan@gmail.com>
2020-03-16 18:41:51 +0200

    added picture-upload possibility

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
